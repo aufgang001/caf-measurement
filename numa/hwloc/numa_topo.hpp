b172f26e508e659b06a1476d54fe5b00ba6b18b4
Sebastian Woelke <Sebastian.Woelke@posteo.de>
2016-11-22 15:12:05 +0100

    Update

---
+++ numa/hwloc/numa_topo.hpp
@@ -0,0 +1,27 @@
+#ifndef NUMA_TOPO_HPP
+#define NUMA_TOPO_HPP
+
+#include <hwloc.h>
+#include <vector>
+#include <functional>
+
+#include "hwloc_bitmap_wrapper.hpp"
+
+class numa_topo {
+public:
+  using pu_id_t = int;
+  using node_id_t = int;
+  using pu_set_t = hwloc_bitmap_wrapper;
+  using node_set_t = hwloc_bitmap_wrapper;
+  using pu_matrix_t = std::vector<pu_set_t>;
+  numa_topo(const hwloc_topology_t topology);
+  pu_matrix_t get_pu_matrix(const pu_set_t& current_pu_id_set) const;
+private:
+  pu_set_t get_pu_set(const node_set_t& node_set) const ;
+  node_set_t get_node_set(const pu_set_t& pu_set) const;
+  const hwloc_distances_s* get_hwloc_distance_matrix() const;
+  const hwloc_topology_t topo_;
+  const hwloc_distances_s* distance_matrix_;
+};
+
+#endif // NUMA_TOPO_HPP
