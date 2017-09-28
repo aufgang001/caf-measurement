bb90ce8813a738bd82fe2d4f469e5edd7518c685
Sebastian Woelke <Sebastian.Woelke@posteo.de>
2017-09-28 22:09:18 +0200

    for testing

--- likwid_node_distance_tester/likwid_node_distnace_tester.py
+++ likwid_node_distance_tester/likwid_node_distnace_tester.py
@@ -20,7 +20,20 @@ def load_dummy_copy_mem_prints(g_a, g_b):
     return [line.strip() for line in f]
 
 def load_copy_mem_prints(g_a, g_b):
-    return execute_cmd("likwid-bench -t copy_mem -w " + g_a + ":1GB-0:" + g_b + ",1:" + g_b)
+    # return execute_cmd("likwid-bench -t copy_mem -w " + g_a + ":1GB-0:" + g_b + ",1:" + g_b) #short
+    return execute_cmd("likwid-bench -t copy_mem -i 1024 -w " + g_a + ":8GB-0:" + g_b + ",1:" + g_b) # long
+
+def load_dummy_matrix():
+    f = open("test_matrix.txt", "r")
+    node_list = f.readline().strip().split()
+    raw_data = []
+    for line in f:
+        raw_data.append(line.strip().split()[1:])
+    data = dict()
+    for zeile_idx, line in zip(range(len(node_list)), raw_data):
+        for spalte_idx, dist in zip(range(len(node_list)), line):
+            data[(node_list[zeile_idx], node_list[spalte_idx])] = int(dist)
+    return (node_list,data)
 
 def get_numa_domains_from_topo(topo_txt):
     result = []
@@ -52,8 +65,20 @@ def plot_throughput_matrix(node_list, data):
             print("%8s" % (data[(zeile, spalte)]), end="") 
         print()
 
+def relativize_matrix(node_list, data_old):
+    # relativized by line
+    data = data_old.copy() 
+    for zeile in node_list:
+        base = float(data[zeile, zeile])
+        data[zeile, zeile] = float(1.000)
+        for spalte in node_list:
+            if zeile != spalte:
+                value = round(base / data[zeile, spalte], 1)
+                data[zeile, spalte] = value
+    return data
+
 def run():
-    data = dict() # key: (a,b), value: Mbyte/s
+    data = dict() # key: (zeile, spalte), value: Mbyte/s
     node_list = get_numa_domains_from_topo(load_topo_prints())
     for a, b in itertools.product(node_list, node_list):
         if (a >= b):
@@ -61,6 +86,13 @@ def run():
             data[(a, b)] = result
             data[(b, a)] = result
     plot_throughput_matrix(node_list, data)
+    print()
+    plot_throughput_matrix(node_list, relativize_matrix(node_list, data))
+
+    # node_list, data = load_dummy_matrix()
+    # plot_throughput_matrix(node_list, data)
+    # print()
+    # plot_throughput_matrix(node_list, relativize_matrix(node_list, data))
 
 if __name__ == "__main__":
     run()
