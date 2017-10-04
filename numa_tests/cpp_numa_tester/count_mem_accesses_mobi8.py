2712626a74d0f5d1486b405bd082cbd74146982a
Sebastian Woelke <Sebastian.Woelke@posteo.de>
2017-10-04 17:28:00 +0200

    for testing

--- numa_tests/cpp_numa_tester/count_mem_accesses_mobi8.py
+++ numa_tests/cpp_numa_tester/count_mem_accesses_mobi8.py
@@ -16,8 +16,10 @@ def execute_cmd(cmd):
     return [line for line in out.split(sep="\n")]
 
 def load_raw_data(cmd):
-    a = "likwid-perfctr -f -O -c 0,16,32,48 -g UNC_CPU_REQUEST_TO_MEMORY_LOCAL_LOCAL_CPU_MEM:UPMC0,UNC_CPU_REQUEST_TO_MEMORY_LOCAL_REMOTE_CPU_MEM:UPMC1 "
-    b = "likwid-perfctr -f -O -c 8,24,40,56 -g UNC_CPU_REQUEST_TO_MEMORY_LOCAL_LOCAL_CPU_MEM:UPMC0,UNC_CPU_REQUEST_TO_MEMORY_LOCAL_REMOTE_CPU_MEM:UPMC1 "
+    a = "likwid-perfctr -f -O -c 0,16,32,48 -g UNC_CPU_REQUEST_TO_MEMORY_LOCAL_LOCAL_CPU_MEM:UPMC0,UNC_CPU_REQUEST_TO_MEMORY_LOCAL_REMOTE_CPU_MEM:UPMC1,UNC_CPU_REQUEST_TO_MEMORY_LOCAL_ANY_CPU_MEM:UPMC2 "
+    b = "likwid-perfctr -f -O -c 8,24,40,56 -g UNC_CPU_REQUEST_TO_MEMORY_LOCAL_LOCAL_CPU_MEM:UPMC0,UNC_CPU_REQUEST_TO_MEMORY_LOCAL_REMOTE_CPU_MEM:UPMC1,UNC_CPU_REQUEST_TO_MEMORY_LOCAL_ANY_CPU_MEM:UPMC2 "
+    # a = "likwid-perfctr -f -O -c 0,16,32,48 -g UNC_CPU_REQUEST_TO_MEMORY_LOCAL_LOCAL_CPU_MEM:UPMC0,UNC_CPU_REQUEST_TO_MEMORY_LOCAL_REMOTE_CPU_MEM:UPMC1 "
+    # b = "likwid-perfctr -f -O -c 8,24,40,56 -g UNC_CPU_REQUEST_TO_MEMORY_LOCAL_LOCAL_CPU_MEM:UPMC0,UNC_CPU_REQUEST_TO_MEMORY_LOCAL_REMOTE_CPU_MEM:UPMC1 "
     raw_data = execute_cmd(a + cmd)
     raw_data += execute_cmd(b + cmd)
     for line in raw_data:
@@ -34,8 +36,10 @@ def get_accesses_per_core(raw_data):
                 counters[dict_value] = int(value_line[column_idx])
     local_counters = dict()
     remote_counters = dict()
+    sum_counters = dict()
     local_data = []
     remote_data = []
+    sum_data = []
     for line in raw_data:
         if "Event,Counter,Core" in line:
             local_data.append(line.split(","))
@@ -44,9 +48,12 @@ def get_accesses_per_core(raw_data):
             local_data.append(line.split(","))
         if "UNC_CPU_REQUEST_TO_MEMORY_LOCAL_REMOTE_CPU_MEM,UPMC1" in line:
             remote_data.append(line.split(","))
+        if "UNC_CPU_REQUEST_TO_MEMORY_LOCAL_ANY_CPU_MEM:UPMC2" in line:
+            sum_data.append(line.split(","))
     fill_counters(local_data, local_counters)
     fill_counters(remote_data, remote_counters)
-    return {"local":local_counters, "remote":remote_counters}
+    fill_counters(sum_data, sum_counters)
+    return {"local":local_counters, "remote":remote_counters, "sum":sum_counters}
 
 def plot_data(data):
     def to_string_in_million(v):
@@ -66,6 +73,8 @@ def plot_data(data):
     print("LOCAL ACCESSES\n" +data_to_string(data["local"]))
     print("")
     print("REMOTE ACCESSES\n" +data_to_string(data["remote"]))
+    print("")
+    print("SUM ACCESSES\n" +data_to_string(data["sum"]))
 
 def load_dummy_raw_data():
     f = open("test_data/plot_a.txt", "r")
