7ea15adfd0f547a975e9ba4f23821a709359052a
Sebastian Woelke <Sebastian.Woelke@posteo.de>
2017-10-27 14:19:04 +0200

    enable plot all via cli arg

--- caf_plot_gui/caf_plot_gui/__main__.py
+++ caf_plot_gui/caf_plot_gui/__main__.py
@@ -1,7 +1,8 @@
 import sys
+import argparse
+from PyQt5 import QtGui, QtWidgets
 from caf_plot_gui import controller
 from caf_plot_gui import model 
-from PyQt5 import QtGui, QtWidgets
 
 config_file = "caf_plot_gui_conf.json"
 
@@ -11,19 +12,27 @@ def usage():
     '''.format(config_file))
     exit()
 
+def parse_all_args():
+    parser = argparse.ArgumentParser(description="CAF Plot Gui") 
+    parser.add_argument("--plot-all", dest="plot_all", action="store_true")
+    parser.add_argument(dest="config_file", nargs="*")
+    return parser.parse_args()
+
 def main():
     global config_file
-    if len(sys.argv) > 2:
-        usage() 
-    if len(sys.argv) == 2:
-        if sys.argv[1] in ["-h", "--help", "-?", "--?"]:
-            usage()
-        else:
-            config_file = sys.argv[1]
-    app = QtWidgets.QApplication(sys.argv)
-    form = controller.controller(model.model(config_file))
-    form.show()
-    app.exec_()
+    args = parse_all_args()
+    if args.plot_all:
+        for file in args.config_file:
+            m = model.model(file)
+            for data in m.plots.values():
+                m.plot_data(data)
+    else:
+        if len(args.config_file) == 1:
+            config_file = args.config_file[0]
+        app = QtWidgets.QApplication(sys.argv)
+        form = controller.controller(model.model(config_file))
+        form.show()
+        app.exec_()
 
 if __name__ == '__main__':
     main()
